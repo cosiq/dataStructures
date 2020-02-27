a00897940400cfade4241bee84f345bcf347ec3b
cosiq <june.namgung0@gmail.com>
2020-02-27 14:12:10 -0800

    QuickSort in Python

--- QuickSort.py
+++ QuickSort.py
@@ -3,6 +3,6 @@ def quickSort(lst):
 	pivot = lst.pop()
 	left, right = [], []
 	for item in lst:
-		if item > pivot: right.append(item)
+		if item >= pivot: right.append(item)
 		if item < pivot: left.append(item)
 	return quickSort(left) + [pivot] + quickSort(right)
